137218d65fa399189d7f289105ae40110d0202f6
deiz0n <eduardomacedosilva@outlook.com>
2024-11-30 11:14:33 -0300

    feat: criação do CreateCategoryUseCaseImpl

---
+++ src/main/java/com/deiz0n/cleanarchitectureexample/adapters/CreateCategoryUseCaseImpl.java
@@ -0,0 +1,20 @@
+package com.deiz0n.cleanarchitectureexample.adapters;
+
+import com.deiz0n.cleanarchitectureexample.core.dtos.CategoryDTO;
+import com.deiz0n.cleanarchitectureexample.core.exceptions.CategoryAlreadyExistException;
+import com.deiz0n.cleanarchitectureexample.core.useCases.CreateCategoryUseCase;
+
+public class CreateCategoryUseCaseImpl implements CreateCategoryUseCase {
+
+    private ProductRepositoryService productRepository;
+
+    @Override
+    public CategoryDTO process(CategoryDTO request) {
+        if (productRepository.doesCategoryNameExist(request.nome()))
+            throw new CategoryAlreadyExistException("Category already registered");
+
+        productRepository.create(request);
+
+        return request;
+    }
+}
