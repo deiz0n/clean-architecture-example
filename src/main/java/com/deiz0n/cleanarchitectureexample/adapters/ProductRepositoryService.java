2dd531f473d0836b01058f1fd2581355cbb44102
deiz0n <eduardomacedosilva@outlook.com>
2024-11-30 11:14:07 -0300

    feat: criação do ProductRepositoryService

---
+++ src/main/java/com/deiz0n/cleanarchitectureexample/adapters/ProductRepositoryService.java
@@ -0,0 +1,13 @@
+package com.deiz0n.cleanarchitectureexample.adapters;
+
+import com.deiz0n.cleanarchitectureexample.core.dtos.CategoryDTO;
+
+import java.util.List;
+
+public interface ProductRepositoryService {
+
+    CategoryDTO create(CategoryDTO request);
+    List<CategoryDTO> getAll();
+    boolean doesCategoryNameExist(String name);
+
+}
